960370ef17915549db7ed5ef9141c4a85940ae1f
nat <nattha.int@gmail.com>
2024-10-14 13:39:42 +0700

    build(auto-release): setup index script execution environment

--- packages/auto-release/src/index.ts
+++ packages/auto-release/src/index.ts
@@ -1,4 +1,9 @@
+#!/usr/bin/env node
+
 export const hello = () => console.log("hello");
 export const addFunction = (a: number, b: number) => a + b;
 
-console.log("hello")
+(() => {
+  console.log("hello version 2")
+})()
+
