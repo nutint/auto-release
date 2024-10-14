8604bf429f4d5459946ce9525838f671873146e7
nat <nattha.int@gmail.com>
2024-10-14 13:33:10 +0700

    build(auto-release): set up cli

--- packages/auto-release/src/commit-parser/__tests__/index.test.ts
+++ packages/auto-release/src/commit-parser/__tests__/index.test.ts
@@ -1,7 +1,7 @@
 import { describe, expect, it } from "vitest";
-import { parseCommit, streamGitLog } from "@/index.js";
 import { InvalidGitLogCommitFormat } from "@/git/git-log";
 import { parseConventionalMessage } from "@/conventional-commit-helper/conventional-commit-helper";
+import {parseCommit, streamGitLog} from "@/commit-parser/commit-parser";
 
 describe("commitParser", () => {
   describe("streamGitLog", () => {
