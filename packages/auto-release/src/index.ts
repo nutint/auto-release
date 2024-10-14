4701cf042de705858fd8e7382f155712670f1ca7
nat <nattha.int@gmail.com>
2024-10-14 13:57:26 +0700

    feat(auto-release): read config file

--- packages/auto-release/src/index.ts
+++ packages/auto-release/src/index.ts
@@ -1,9 +1,27 @@
 #!/usr/bin/env node
 
+import fs from "fs"
+import path from "path"
+
 export const hello = () => console.log("hello");
 export const addFunction = (a: number, b: number) => a + b;
 
 (() => {
-  console.log("hello version 2")
+  const args = process.argv.slice(2)
+  const configFilePath = args.find(arg => arg.includes("--config"))?.split("=")[1]
+
+  if(configFilePath === undefined) {
+    console.error("Please provide a config file using --config")
+    process.exit(1)
+  }
+
+  const absolutePath = path.resolve(process.cwd(), configFilePath)
+  if(fs.existsSync(absolutePath)) {
+    const config = JSON.parse((fs.readFileSync(absolutePath, "utf-8")))
+    console.log("Configuration Loaded", config)
+  } else {
+    console.error("Config file not found: ", configFilePath)
+    process.exit(1)
+  }
 })()
 
