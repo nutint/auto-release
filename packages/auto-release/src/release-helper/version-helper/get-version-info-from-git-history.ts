7fa1d7558224b66d9c4e338226ac272b5d90632a
nat <nattha.int@gmail.com>
2024-11-02 14:49:06 +0700

    refactor(auto-release): introduce operation for extracting git commits

--- packages/auto-release/src/release-helper/version-helper/get-version-info-from-git-history.ts
+++ packages/auto-release/src/release-helper/version-helper/get-version-info-from-git-history.ts
@@ -1,7 +1,16 @@
 import { gitHelper } from "@/git/git-helper";
 import { createLogConfig } from "@/release-helper/release-helper";
-import { filter, lastValueFrom, map, mergeMap, toArray } from "rxjs";
+import {
+  filter,
+  lastValueFrom,
+  map,
+  mergeMap,
+  Observable,
+  toArray,
+} from "rxjs";
 import * as semver from "semver";
+import { MappedCommit } from "@/git/git-log";
+import { ConventionalCommit } from "@/conventional-commit-helper/conventional-commit-helper";
 
 type VersionInfo = {
   latestTags: string[];
@@ -13,13 +22,12 @@ type GetVersionParams = {
   gitTagPrefix?: string;
 };
 
-export const getVersionInfoFromGitHistory = async ({
-  scope,
-  gitTagPrefix,
-}: GetVersionParams): Promise<VersionInfo> => {
-  const $result = gitHelper()
-    .getLogStream(createLogConfig({ scope }))
-    .pipe(
+const extractValidTags =
+  (gitTagPrefix?: string) =>
+  (
+    source: Observable<MappedCommit<ConventionalCommit>>,
+  ): Observable<VersionInfo> =>
+    source.pipe(
       mergeMap((commit) => commit.tags),
       filter((tag) =>
         gitTagPrefix !== undefined ? tag.startsWith(`${gitTagPrefix}@`) : true,
@@ -29,17 +37,28 @@ export const getVersionInfoFromGitHistory = async ({
       ),
       filter((tag) => semver.valid(tag) !== null),
       toArray(),
+      map((tags) => {
+        const latestTags = tags.sort(semver.compare);
+        return {
+          latestTags: latestTags.map((ver) =>
+            gitTagPrefix !== undefined ? `${gitTagPrefix}@${ver}` : ver,
+          ),
+          latestStableTags: latestTags
+            .filter((tag) => !semver.prerelease(tag))
+            .map((ver) =>
+              gitTagPrefix !== undefined ? `${gitTagPrefix}@${ver}` : ver,
+            ),
+        };
+      }),
     );
 
-  const latestTags = (await lastValueFrom($result)).sort(semver.compare);
-  return {
-    latestTags: latestTags.map((ver) =>
-      gitTagPrefix !== undefined ? `${gitTagPrefix}@${ver}` : ver,
-    ),
-    latestStableTags: latestTags
-      .filter((tag) => !semver.prerelease(tag))
-      .map((ver) =>
-        gitTagPrefix !== undefined ? `${gitTagPrefix}@${ver}` : ver,
-      ),
-  };
+export const getVersionInfoFromGitHistory = async ({
+  scope,
+  gitTagPrefix,
+}: GetVersionParams): Promise<VersionInfo> => {
+  const $result = gitHelper()
+    .getLogStream(createLogConfig({ scope }))
+    .pipe(extractValidTags(gitTagPrefix));
+
+  return await lastValueFrom($result);
 };
